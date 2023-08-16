b230d0505dc7f5436d6a425b085c4312f5197d85
João Paulo <joao.paulomonteiro@hotmail.com>
2023-08-15 22:12:43 -0300

    configuring localstorage

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,42 @@
 import { BrowserRouter } from "react-router-dom";
 
+import { useAuth } from "../hooks/auth";
+
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+
+  const { user } = useAuth();
+
+  // console.log(user)
+
+  // if(user){
+  //   if(user.isAdmin == true) {
+  //     return (
+  //     <BrowserRouter>
+  //       <AppRoutes />
+  //     </BrowserRouter>
+  //     )
+  //   } else {
+  //     return (
+  //     <BrowserRouter>
+  //       <AppRoutes />
+  //     </BrowserRouter>
+  //     )
+  //   } 
+  // } else {
+  //   return (
+  //   <BrowserRouter>
+  //     <AuthRoutes />
+  //   </BrowserRouter>
+  //   )
+  // }
+  
+
   return(
     <BrowserRouter>
-      <AuthRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   )
 }
